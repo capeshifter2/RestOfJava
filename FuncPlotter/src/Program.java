bf093fca2cd3180d98e619eb8918e2d90221bdf8
s-daroy <s-daroy@484E293884.lwsd.k12wa.org>
2022-05-16 13:52:42 -0700

    Added more math functions. Log still needs fixing

--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Scanner;
 
 import graphics.Canvas;
 
@@ -14,16 +15,54 @@ public class Program {
         canvas.setRange(-360, -240, 360, 240);
         canvas.open();
 
-        // Draw a short red diagonal on the canvas
+        getInput();
 
-        canvas.setColor(Color.red);
-        MyFunction fSin =  (x)->{return (int)(200 * Math.sin(Math.PI*x/360));};
-        for (int x = -180; x < 360 + 180; x++) {
-            canvas.plot(x - 180, fSin.calcY(x));
-        }
+        
 
         // Pause and close the canvas then terminate the program.
         canvas.pause();
         canvas.close();
+    }
+    private static void getInput() {
+    	Scanner input = new Scanner(System.in);
+    	boolean exit = false;
+    	while (exit == false) {
+	    	String in = input.next();
+	    	switch (in) {
+	    	case "S":
+	    		sin();
+	    		break;
+	    	case "Q":
+	    		quad();
+	    		break;
+	    	case "L":
+	    		Log();
+	    		break;
+			default:
+				
+				break;
+	    	}
+    	}
+    }
+    private static void sin() {
+    	canvas.setColor(Color.red);
+        MyFunction fSin =  (x)->{return (int)(200 * Math.sin(Math.PI*x/360));};
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, fSin.calcY(x));
+        }
+    }
+    private static void quad() {
+    	canvas.setColor(Color.blue);
+    	MyFunction fq =  (x)->{return (int)(x * x) / 100;};
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, fq.calcY(x));
+        }
+    }
+    private static void Log() {
+    	canvas.setColor(Color.blue);
+    	MyFunction flog =  (x)->{return (int)(Math.log(x)) / 100;};
+        for (int x = -360; x < 360; x++) {
+            canvas.plot(x, flog.calcY(x));
+        }
     }
 }
